9b807d124660836bb4ae73c854996659d4d37ac5
Kris Rusin <kris.rusin@gmail.com>
2022-10-19 00:02:29 -0500

    Updates

---
+++ precache-manifest.fe5740d8dd1166190db9b7fd6b10830c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cc2d9be02cd1fa88bfa",
+    "url": "/index.html/static/css/main.49e21e2f.chunk.css"
+  },
+  {
+    "revision": "0cc2d9be02cd1fa88bfa",
+    "url": "/index.html/static/js/main.0cc2d9be.chunk.js"
+  },
+  {
+    "revision": "2c02afda2375985545ff",
+    "url": "/index.html/static/js/1.2c02afda.chunk.js"
+  },
+  {
+    "revision": "6c09c7ac48f2a3125bc4",
+    "url": "/index.html/static/js/runtime~main.6c09c7ac.js"
+  },
+  {
+    "revision": "31453f10de5790fa7f11e22a004d198d",
+    "url": "/index.html/index.html"
+  }
+];
