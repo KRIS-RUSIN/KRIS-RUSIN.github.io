d9aa8546f1094d35293135b32b1771c77d86b101
Kris Rusin <kris.rusin@gmail.com>
2022-10-19 00:57:07 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "www.krisrusin.com/precache-manifest.52c5a50a32691fe287cd3bc50f828edd.js"
+  "/precache-manifest.5d4c6c67f7cb06b96cef8493bef16d1b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("www.krisrusin.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
