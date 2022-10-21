8658fbcebd91ca5a9f559af9ea79623749b5cbb4
Krzysztof Rusin <56460931+KRIS-RUSIN@users.noreply.github.com>
2022-10-21 14:38:07 -0500

    Updates

---
+++ precache-manifest.32cadcc3631239a4ce010498adc13281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90af887c79739310755b",
+    "url": "/static/css/main.b8005636.chunk.css"
+  },
+  {
+    "revision": "90af887c79739310755b",
+    "url": "/static/js/main.5815fb62.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4b79856004bb3c554c2",
+    "url": "/static/js/2.2ce2ee5f.chunk.js"
+  },
+  {
+    "revision": "03433ebc3e7bfb72489804b4d53db4cd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "www.krisrusin.com/precache-manifest.a0eb3f2d7afdd4127605ce88e88c78a2.js"
+  "/precache-manifest.32cadcc3631239a4ce010498adc13281.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("www.krisrusin.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
