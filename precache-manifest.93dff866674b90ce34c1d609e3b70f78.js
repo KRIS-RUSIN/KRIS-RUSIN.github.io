edd5d1610f668bd62de76ec4cb0adeff5e05e7f8
Krzysztof Rusin <56460931+KRIS-RUSIN@users.noreply.github.com>
2022-10-21 14:31:00 -0500

    Updates

---
+++ precache-manifest.93dff866674b90ce34c1d609e3b70f78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90af887c79739310755b",
+    "url": "www.krisrusin.com/index.html/static/css/main.b8005636.chunk.css"
+  },
+  {
+    "revision": "90af887c79739310755b",
+    "url": "www.krisrusin.com/index.html/static/js/main.5815fb62.chunk.js"
+  },
+  {
+    "revision": "25f02877c58fac8ab994",
+    "url": "www.krisrusin.com/index.html/static/js/runtime~main.7ee3adde.js"
+  },
+  {
+    "revision": "b4b79856004bb3c554c2",
+    "url": "www.krisrusin.com/index.html/static/js/2.2ce2ee5f.chunk.js"
+  },
+  {
+    "revision": "cbb06812564e5c6375b3b900c5fdffeb",
+    "url": "www.krisrusin.com/index.html/index.html"
+  }
+];
