aec55f1831a28d819b7991039e47b7d254aacb42
Krzysztof Rusin <56460931+KRIS-RUSIN@users.noreply.github.com>
2023-01-26 22:24:41 -0600

    about me page

--- src/resources.js
+++ src/resources.js
@@ -1,4 +1,10 @@
 export const resources = {
+    ABOUT_ME: "I am a software engineer with a passion for technology and outdoor activities. As a software engineer, "
+    + "I am constantly seeking to expand my knowledge and develop new skills. I am well-versed in programming languages such as C#, "
+    + "TypeScript and React and have experience with web development. I am familiar with agile methodologies and am comfortable working in " 
+    + "a fast-paced environment. I am excited to begin my career as a software engineer and eager to take on new challenges. "
+    + "I am confident in my ability to make a positive impact in the industry with my dedication to learning and collaborative working style. "
+    + "In my free time, I enjoy snowboarding, biking, and swimming which have helped me develop discipline, determination, and teamwork skills. ",
     INTRO: "Hi, I'm Krzysztof, a Software Engineer in the Chicagoland Area",
     SOCIAL_MEDIA_NAMES : {
         LINKEDIN : "LinkedIn",
